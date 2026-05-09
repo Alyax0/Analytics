0ef53d7bb70481ee56c84dc3c0a1e0709d8e7901
Alyax0 <alessandroaxespinosanavarro@gmail.com>
2026-05-09 16:16:00 -0600

    Update nba.js

--- api/nba.js
+++ api/nba.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   if (!path) return res.status(400).json({ error: 'Missing path' });
 
   try {
-    const url = 'https://api.balldontlie.io/v1/' + decodeURIComponent(path);
+    const url = 'https://api.balldontlie.io/v1/' + path;
     const response = await fetch(url, {
       headers: {
         'Authorization': 'b922db60-e2d6-4de6-b156-766b691e668a'
